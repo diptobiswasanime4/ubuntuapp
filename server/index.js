1671087d15686bf4387c92ce07ca1121c9858c1f
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-11-28 02:10:32 +0530

    📦 profile route added

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.json({ msg: "Home Page", success: true });
 });
 
+app.get("/profile", (req, res) => {
+  res.json({ msg: "Profile not found", success: false });
+});
+
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}...`);
 });
