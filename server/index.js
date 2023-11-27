f0a92cffe751f4e0fb880d311a1d97cb60207ccd
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-11-28 02:17:30 +0530

    📦 updates

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Home Page", success: true });
+  res.json({ msg: "Home Page.", success: true });
 });
 
 app.get("/profile", (req, res) => {
-  res.json({ msg: "Profile not found", success: false });
+  res.json({ msg: "Profile not found!", success: false });
 });
 
 app.listen(PORT, () => {
